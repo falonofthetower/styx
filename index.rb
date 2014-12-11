f5120354818c39f790effbb512b599b14647d8ab
Peter Karth <falonofthetower@gmail.com>
2014-12-10 22:30:06 -0600

    changed round length back from testing

--- index.rb
+++ index.rb
@@ -12,7 +12,7 @@ begin
   rock_paper_scissors.player_chooses_weapon
   computer.weapon = rock_paper_scissors.computer_chooses_weapon
   rock_paper_scissors.compare_weapons(user,computer)
-end until user.wins >= 1
+end until user.wins >= 10
 rock_paper_scissors.conclusion
 
 # Tic Tac Toe Round
@@ -38,7 +38,7 @@ begin
   tictactoe.process_winner
   tictactoe.declare_winner  
   starting_player = starting_player * -1
-end until user.wins >= 2
+end until user.wins >= 20
 tictactoe.conclusion
 
 # Black Jack Round
